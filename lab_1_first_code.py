bde2f460baabc04124bcda46874b9d7d7cb9d439
sscott11895 <102761901+sscott11895@users.noreply.github.com>
2022-03-31 15:43:59 -0500

    create first code

---
+++ lab_1_first_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 31 15:40:43 2022
+
+@author: scotty
+"""
+
+print('Aditi S is my favorite person. JK')
+
+x = 7
+print (x*x)
